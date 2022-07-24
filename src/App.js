180e5dc3f9525b36aaee9ac5627b4d8789b8fbc7
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-07-24 18:33:16 +0100

    adding state to App component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
-        </div>
-    );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        };
+    }
+    
+    render() {
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox />
+                <CardList robots={robots} />
+            </div>
+        );
+    }
 }
 
 export default App;
