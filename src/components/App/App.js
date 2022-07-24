e940a377f5d8f57ea82c105a87a6e9efd8e69fca
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-07-24 19:22:30 +0100

    adding loading text

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -28,13 +28,17 @@ class App extends React.Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         });
 
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className='tc f1'>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        };
     }
 }
 
