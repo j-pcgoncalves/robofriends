f1ebec405a4777346f57e1c195707a3f3e102970
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2022-07-24 17:37:33 +0100

    choosing the cards style

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div>
+        <div className='bg-light-green dib hr3 pa3 ma2 grow bw2 shadow-3'>
             <img src='https://robohash.org/test?200x200' alt='robots' />
             <div>
                 <h2>Jane Doe</h2>
