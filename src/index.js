bd1b576cd621363f3c26d4e332f455d1308d7839
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-02-15 12:50:52 +0000

    create redux store and provider, correct constants typo, update README

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './containers/App';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from "redux";
 import "tachyons";
 
+import './index.css';
+import App from './containers/App';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 
