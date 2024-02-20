45376d03bb0d3e29363fc0e619e38d07b748a03b
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2024-02-20 22:05:54 +0000

    make changes to improve lighthouse score

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,7 +1,8 @@
 const SearchBox = ({ searchChange }) => {
     return (
         <div className="pa2">
-            <input 
+            <input
+                aria-label="Search Robots" 
                 className="pa3 ba b--green bg-lightest-blue"
                 type="search"
                 placeholder="Search Robots"
